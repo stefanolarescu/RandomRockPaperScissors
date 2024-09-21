e573b043d01cb22ab787a8d839f450bc0a01d41a
Stefan Olarescu <stefanolarescu@gmail.com>
2024-09-21 22:10:53 +0300

    Created Xcode project.

---
+++ RandomRockPaperScissors/RandomRockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomRockPaperScissorsApp.swift
+//  RandomRockPaperScissors
+//
+//  Created by Stefan Olarescu on 21.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct RandomRockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
